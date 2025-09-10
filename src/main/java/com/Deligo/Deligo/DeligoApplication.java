7e15437422d917f50b7fab252b1240fce8486dd9
dileepkuamar <chukkala.dileepkumar@gmail.com>
2025-09-10 14:43:06 +0530

    Deligo Food Delivery App

---
+++ src/main/java/com/Deligo/Deligo/DeligoApplication.java
@@ -0,0 +1,13 @@
+package com.Deligo.Deligo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeligoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeligoApplication.class, args);
+	}
+
+}
